5cee3d806ca99d07226cf813e1ace0d8c4b8b18d
chhaichivon <chhaichivon1995@gmail.com>
2017-10-01 14:10:09 +0700

    crud category

--- api/controllers/CategoriesController.js
+++ api/controllers/CategoriesController.js
@@ -4,8 +4,25 @@
  * @description :: Server-side logic for managing categories
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
-
+var pager = require('sails-pager');
 module.exports = {
+    list:function(req, res){
+        var perPage = req.query.per_page;
+        var currentPage = req.query.page;
+        var conditions = {active: true};
+
+        Categories.find({
+            skip: perPage,
+            limit: perPage,
+            sort: 'createdAt DESC'
+        }).exec(function(err, categories){
+            pager.paginate(categories, conditions, currentPage, perPage, null, 'createdAt DESC').then(function(records){
+                return records;
+            }).catch(function(err) {
+                return err;
+            });
+        });
+    }
 	
 };
 
--- api/models/Categories.js
+++ api/models/Categories.js
@@ -8,7 +8,12 @@
 module.exports = {
 
   attributes: {
-
+    name:{
+      type: 'string'
+    },
+    description:{
+      type:'string'
+    }
   }
 };
 
