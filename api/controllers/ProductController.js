b9db52001af37077c24a0a7d461ad1f65d1185b5
chhaichivon <chivonchhai@hotmail.com>
2017-10-14 22:48:55 +0700

    update css header

--- api/controllers/ProductController.js
+++ api/controllers/ProductController.js
@@ -9,29 +9,35 @@ module.exports = {
     /**
      * `ProductController.findall()`
      */
-    findall: function (req, res) {
-        console.log("Inside findall..............");
-        var page = req.param("page");
-        var limit = req.param("limit");
-        return Product.find().then(function (products) {
-            console.log("fin all product = " , res.json(products));
-            return res.json(products);
-        }).catch(function (err) {
-            console.error("Error on find all", err);
-        });
+    findall: function (req, res,next) {
+        var p = req.param("page");
+        if(p == null){
+            p = 1;
+        }
+        var l = req.param("limit");
+        if(l== null){
+            l = 10;
+        }
+        return Product.find({ skip: p,
+            limit: l}).exec(function(err, products) {
+            if(err) throw err;
+            next(products);
+            return {
+                    "data":products,
+                    "page":p,
+                    "limit":l
+                }
+          });
     },
     /**
      * `ProductController.find()`
      */
     find: function (req, res) {
         var pId = req.params.id;
-        console.log("Inside find.............." , pId);
-
         return Product.find({id : pId}).then(function (product) {
-            console.log("fin all product = " ,  res.json(product));
             return res.json(product);
         }).catch(function (err) {
-            console.error("Error on find all", err);
+            console.log("Error on find all", err);
         });
     },
     /**
@@ -44,10 +50,9 @@ module.exports = {
             imageUrl: req.param("imageUrl")
         };
         return Product.create(product).then(function (pro) {
-                     console.log("Product create: " ,  res.json(pro));
                      return res.json(pro);
                 }).catch(function (err) {
-                        console.error(err);
+                        console.log(err);
                 });
     },
     /**
@@ -55,17 +60,15 @@ module.exports = {
      */
     update: function (req, res) {
         var pId = req.params.id;
-        console.log("Inside update..............", pId);
         var product = {
             name: req.param("name"),
             description: req.param("description"),
             imageUrl: req.param("imageUrl")
         };
         return Product.update({id: pId}, product).then(function (product) {
-                    console.log("Deleted successfully!!!  = " ,  res.json(product));
                     return res.json(product);
                 }).catch(function (err) {
-                    console.log("Deleted unsuccessfully");
+                    console.log(err);
                 });
 
      },
@@ -74,13 +77,10 @@ module.exports = {
      */
     delete: function (req, res) {
         var pId = req.params.id;
-        console.log("Inside delete..............");
-
         return Product.destroy({ id: pId }).then(function (product) {
-            console.log("Deleted successfully!!!  = " , res.json(product));
             return res.json(product);
         }).catch(function (err) {
-            console.log("Deleted unsuccessfully");
+            console.log(err);
         });
 
     }
